80a74e798c1c2fb53f1d03b99db334328e15e439
drib14 <jhondribramirez7@gmail.com>
2026-05-31 15:24:46 +0800

    refactor: remove deprecated legacy configuration module

--- backend/models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const messageSchema = mongoose.Schema({
-    booking: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Message', messageSchema);
